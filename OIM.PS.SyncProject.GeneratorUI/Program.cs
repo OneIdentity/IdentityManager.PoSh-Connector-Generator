feff257fe1bd22b546acc34e1ed71b10ae4f5801
pvsherman <pvsherman@yahoo.com>
2023-09-25 11:24:43 -0700

    Small change

--- OIM.PS.SyncProject.GeneratorUI/Program.cs
+++ OIM.PS.SyncProject.GeneratorUI/Program.cs
@@ -9,7 +9,7 @@ namespace OIM.PS.SyncProject.GeneratorUI
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///  The main entry point for the application!
         /// </summary>
         [STAThread]
         static void Main()
